d72025b3ab41828b60c060137444f113e29cef4e
Konanets <ivankonanezs4@gmail.com>
2022-08-10 02:08:07 +0300

    адішин

--- lesson6/script.js
+++ lesson6/script.js
@@ -1,24 +1,24 @@
-let words=['hello world', 'lorem ipsum', 'javascript is cool'];
+let words = ['hello world', 'lorem ipsum', 'javascript is cool'];
 
 // - Знайти та вивести довижину настипних стрінгових значень
 // 'hello world', 'lorem ipsum', 'javascript is cool'
-words.forEach(str=> console.log(str.length))
+words.forEach(str => console.log(str.length))
 
 
 // - Перевести до великого регістру наступні стрінгові значення
 // 'hello world', 'lorem ipsum', 'javascript is cool'
-let upperWords=words.map(str=> {
+let upperWords = words.map(str => {
     console.log(str.toUpperCase())
     return str.toUpperCase()
 })
 
 // - Перевести до нижнього регістру настипні стрінгові значення
 // 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
-upperWords.forEach(str=> console.log(str.toLowerCase()))
+upperWords.forEach(str => console.log(str.toLowerCase()))
 
 // - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
 let dirty = ' dirty string   ';
-clear=dirty.trim();
+clear = dirty.trim();
 console.log(clear);
 
 
@@ -27,15 +27,15 @@ console.log(clear);
 
 let str = 'Ревуть воли як ясла повні';
 
-const stringToarray=str=>str.split(' ');
+const stringToarray = str => str.split(' ');
 
 let arr = stringToarray(str); //['Ревуть', 'воли', 'як', 'ясла', 'повні']
 console.log(arr);
 
 //
 // - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
-let numbers=[10,8,-7,55,987,-1011,0,1050,0];
-numbers=numbers.map(num=>num.toString())
+let numbers = [10, 8, -7, 55, 987, -1011, 0, 1050, 0];
+numbers = numbers.map(num => num.toString())
 console.log(numbers);
 
 
@@ -44,14 +44,14 @@ console.log(numbers);
 //     let nums = [11,21,3];
 // sortNums(nums,'ascending') // [3,11,21]
 // sortNums(nums,'descending') // [21,11,3]
-let sortNums=(numbers,type)=>{
-    if(type==='ascending')numbers.sort((number1,number2)=>number1-number2);
-    else if(type==='descending')numbers.sort((number1,number2)=>number2-number1);
+let sortNums = (numbers, type) => {
+    if (type === 'ascending') numbers.sort((number1, number2) => number1 - number2);
+    else if (type === 'descending') numbers.sort((number1, number2) => number2 - number1);
     return numbers;
 }
-let nums = [11,21,3];
-console.log(sortNums(nums,'ascending') )
-console.log(sortNums(nums,'descending') )
+let nums = [11, 21, 3];
+console.log(sortNums(nums, 'ascending'))
+console.log(sortNums(nums, 'descending'))
 
 // - є масив
 let coursesAndDurationArray = [
@@ -64,12 +64,12 @@ let coursesAndDurationArray = [
 ];
 
 // -- відсортувати його за спаданням за monthDuration
-    coursesAndDurationArray.sort((item1,item2)=>item1.monthDuration-item2.monthDuration)
-    console.log(coursesAndDurationArray);
+coursesAndDurationArray.sort((item1, item2) => item1.monthDuration - item2.monthDuration)
+console.log(coursesAndDurationArray);
 
 
 // -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
-coursesAndDurationArray.filter((item)=>item.monthDuration>5)
+coursesAndDurationArray.filter((item) => item.monthDuration > 5)
 console.log(coursesAndDurationArray);
 
 
@@ -87,206 +87,206 @@ console.log(coursesAndDurationArray);
 //         value: '', // '6'-'10', 'ace','jack','queen','king','joker'
 //     color:'', // 'red','black'
 // }
-let deckOfCards=[
+let deckOfCards = [
     //diamond
     {
         cardSuit: 'diamond',
         value: '6',
-        color:'red',
+        color: 'red',
     },
     {
         cardSuit: 'diamond',
         value: '7',
-        color:'red',
+        color: 'red',
     },
     {
         cardSuit: 'diamond',
         value: '8',
-        color:'red',
+        color: 'red',
     },
     {
         cardSuit: 'diamond',
         value: '9',
-        color:'red',
+        color: 'red',
     },
     {
         cardSuit: 'diamond',
         value: '10',
-        color:'red',
+        color: 'red',
     },
     {
         cardSuit: 'diamond',
         value: 'ace',
-        color:'red',
+        color: 'red',
     },
     {
         cardSuit: 'diamond',
         value: 'jack',
-        color:'red',
+        color: 'red',
     },
     {
         cardSuit: 'diamond',
         value: 'queen',
-        color:'red',
+        color: 'red',
     },
     {
         cardSuit: 'diamond',
         value: 'king',
-        color:'red',
+        color: 'red',
     },
     //heart
     {
         cardSuit: 'heart',
         value: '6',
-        color:'red',
+        color: 'red',
     },
     {
         cardSuit: 'heart',
         value: '7',
-        color:'red',
+        color: 'red',
     },
     {
         cardSuit: 'heart',
         value: '8',
-        color:'red',
+        color: 'red',
     },
     {
         cardSuit: 'heart',
         value: '9',
-        color:'red',
+        color: 'red',
     },
     {
         cardSuit: 'heart',
         value: '10',
-        color:'red',
+        color: 'red',
     },
     {
         cardSuit: 'heart',
         value: 'ace',
-        color:'red',
+        color: 'red',
     },
     {
         cardSuit: 'heart',
         value: 'jack',
-        color:'red',
+        color: 'red',
     },
     {
         cardSuit: 'heart',
         value: 'queen',
-        color:'red',
+        color: 'red',
     },
     {
         cardSuit: 'heart',
         value: 'king',
-        color:'red',
+        color: 'red',
     },
     //clubs
     {
         cardSuit: 'clubs',
         value: '6',
-        color:'black',
+        color: 'black',
     },
     {
         cardSuit: 'clubs',
         value: '7',
-        color:'black',
+        color: 'black',
     },
     {
         cardSuit: 'clubs',
         value: '8',
-        color:'black',
+        color: 'black',
     },
     {
         cardSuit: 'clubs',
         value: '9',
-        color:'black',
+        color: 'black',
     },
     {
         cardSuit: 'clubs',
         value: '10',
-        color:'black',
+        color: 'black',
     },
     {
         cardSuit: 'clubs',
         value: 'ace',
-        color:'black',
+        color: 'black',
     },
     {
         cardSuit: 'clubs',
         value: 'jack',
-        color:'black',
+        color: 'black',
     },
     {
         cardSuit: 'clubs',
         value: 'queen',
-        color:'black',
+        color: 'black',
     },
     {
         cardSuit: 'clubs',
         value: 'king',
-        color:'black',
+        color: 'black',
     },
     // spade
     {
         cardSuit: 'spade',
         value: '6',
-        color:'black',
+        color: 'black',
     },
     {
         cardSuit: 'spade',
         value: '7',
-        color:'black',
+        color: 'black',
     },
     {
         cardSuit: 'spade',
         value: '8',
-        color:'black',
+        color: 'black',
     },
     {
         cardSuit: 'spade',
         value: '9',
-        color:'black',
+        color: 'black',
     },
     {
         cardSuit: 'spade',
         value: '10',
-        color:'black',
+        color: 'black',
     },
     {
         cardSuit: 'spade',
         value: 'ace',
-        color:'black',
+        color: 'black',
     },
     {
         cardSuit: 'spade',
         value: 'jack',
-        color:'black',
+        color: 'black',
     },
     {
         cardSuit: 'spade',
         value: 'queen',
-        color:'black',
+        color: 'black',
     },
     {
         cardSuit: 'spade',
         value: 'king',
-        color:'black',
+        color: 'black',
     },
 
-    // Джокерів тільки 2
+    // Джокери
     {
         cardSuit: '-',
         value: 'joker',
-        color:'black',
+        color: 'black',
     },
     {
         cardSuit: '-',
         value: 'joker',
-        color:'red',
+        color: 'red',
     },
 ]
 // - знайти піковий туз
-console.log(deckOfCards.find((item) => item.cardSuit === 'spade' && item.value==='ace'));
+console.log(deckOfCards.find((item) => item.cardSuit === 'spade' && item.value === 'ace'));
 // - всі шістки
 console.log(deckOfCards.filter((item) => item.value === '6'));
 
@@ -296,8 +296,7 @@ console.log(deckOfCards.filter((item) => item.color === 'red'));
 // - всі буби
 console.log(deckOfCards.filter(item => item.cardSuit === 'diamond'));
 // - всі трефи від 9 та більше
-console.log(deckOfCards.filter(item => item.cardSuit === 'clubs'&& !(item.value<=9)));
-
+console.log(deckOfCards.filter(item => item.cardSuit === 'clubs' && !(item.value <= 9)));
 
 
 // Додатково по reduce
@@ -309,13 +308,19 @@ console.log(deckOfCards.filter(item => item.cardSuit === 'clubs'&& !(item.value<
 //     clubs:[]
 // }
 
-let box= deckOfCards.reduce((cardsBlock,card)=>{
-    if(card.cardSuit==='spade')cardsBlock.spades.push(card)
-    else if(card.cardSuit==='diamond')cardsBlock.diamonds.push(card)
-    else if(card.cardSuit==='heart')cardsBlock.hearts.push(card)
-    else if(card.cardSuit==='clubs')cardsBlock.clubs.push(card)
+let box = deckOfCards.reduce((cardsBlock, card) => {
+    if (card.cardSuit === 'spade') cardsBlock.spades.push(card)
+    else if (card.cardSuit === 'diamond') cardsBlock.diamonds.push(card)
+    else if (card.cardSuit === 'heart') cardsBlock.hearts.push(card)
+    else if (card.cardSuit === 'clubs') cardsBlock.clubs.push(card)
     else cardsBlock.other.push(card)
     return cardsBlock;
-},{spades:[],diamonds:[],hearts:[],clubs:[],other:[]})
+}, {spades: [], diamonds: [], hearts: [], clubs: [], other: []})
 
 console.log(box);
+
+
+
+
+
+
---
+++ lesson6/Аддішин/index.js
@@ -0,0 +1,258 @@
+// - Напишите функцию cutString(str, n), которая делит строку на подстроки, состоящие из n символов.
+// document.writeln(cutString('наслаждение',3)) // [нас,лаж,ден,ие]
+function cutString(str, n){
+    let arrResult = [];
+    for(let i = 0 ; i < str.length; i += n) {
+        arrResult.push(str.substr(i, n));
+    }
+    return  arrResult;
+}
+
+console.log(cutString('I believe in what is not, was not and cannot be', 5));
+
+
+// - Напишіть функцію delete_characters(str, length), яка повертає підрядок, що складається із зазначеної кількості символів.
+//     let str = 'Каждый охотник желает знать';
+// document.writeln(delete_characters(str, 7)); // охотник
+
+function delete_characters(str, length){
+    return str.split(' ').find(word=>word.length===length);
+}
+document.writeln(delete_characters('Каждый охотник желает знать', 7));
+
+
+// - Напишіть функцію insert_dash(str), яка приймає рядок str як аргумент і вставляє тире (-) між словами. При цьому всі символи рядка необхідно перевести у верхній регістр.
+//     let str = "HTML JavaScript PHP";
+// document.writeln(insert_dash(str)); // 'HTML-JAVASCRIPT-PHP'
+
+function insert_dash(str){
+    return str.toUpperCase().split(' ').join('-')
+}
+    let str = "HTML JavaScript PHP";
+console.log(insert_dash(str)); // 'HTML-JAVASCRIPT-PHP'
+
+
+//
+// - Напишіть функцію, яка приймає рядок як аргумент і перетворює регістр першого символу рядка з нижнього регістру у верхній.
+
+
+function capitalizeFirstLetterOne(str){
+    return str[0].toUpperCase().concat(str.slice(1))
+}
+console.log(capitalizeFirstLetterOne('history'));
+
+// - Дано список імен.
+//     let n1 = 'Harry..Potter'
+// let n2 = 'Ron---Whisley'
+// let n3 = 'Hermione__Granger'
+// Написати функцію, яка приймає будь яке не валідне імя, та нормалізує його в наступнйи вигляд
+// let n1 = 'Harry Potter'
+// let n2 = 'Ron Whisley'
+// let n3 = 'Hermione Granger'
+function normalizeStr(str){
+    let name = ''
+    for (let letter of str) {
+        if (letter.toLowerCase() !== letter.toUpperCase()) {
+            name += letter;
+        } else {
+            if (name[name.length - 1] !== ' ') {
+                name += ' ';
+            }
+        }
+    }
+    return name;
+}
+console.log(normalizeStr('Harry..Potter'));
+console.log(normalizeStr('Ron---Whisley'));
+console.log(normalizeStr('Hermione__Granger'));
+
+//
+//     - створити функцію, яка генерує масив рандомних числових цілих значень в діапазоні від 0 до 100.
+    function random(){
+        let array=[];
+        for (let i = 100; i!==0 ; i--) {
+            array.push(Math.floor(Math.random()*100))
+        }
+        return array;
+    }
+console.log(random())
+// - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. Відсортувати його за допомоги sort
+console.log(random().sort((num1, num2) => num1 - num2));
+// - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. відфільтрувати  його за допомоги filter, залишивши тільки парні числа (без 0!)
+console.log(random().filter((num1) => num1>0&&num1%2===0));
+
+
+
+
+
+// - Напишіть функцію capitalize(str), яка повертає рядок, у якому кожне слово починається з великої літери.
+function capitalizeAllFirstLetter(str){
+
+    return (str.split(' ').map(item => item[0].toUpperCase().concat(item.slice(1)))).join(' ')
+}
+console.log(capitalizeAllFirstLetter('some day'));
+
+
+// - Створити функцію-валідатор для адрес електронної пошти. Перевірка повинна включати в себе :данні до знака равлика(@),
+// наявність равлика, крапку яка знаходиться не меньше ніж на 2 символ далі після равлика, функція не чутлива до регістру
+// (some@email.com,SOME@EMAIL.COM,some@EMAIL.com, і тд - однакові значення)
+// Протестувати на значеннях
+// someemail@gmail.com
+// someeMAIL@gmail.com
+// someeMAIL@i.ua
+// some.email@gmail.com
+// Примітка
+// Для тих, хто дуже розумний, та почне використовувати регулярні вирази одразу "ні".
+// Своїм мозком подумайте над протоколом, з регулярками будете потім бавитись.
+let validator = (email)=>{
+
+    //Варіант з регулярками (Це просто красиво)
+     //const email_reg=/^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+     //return email_reg.test(email)?'Good Address':'Wrong Address'
+
+    //Варіант 1
+    // let valid = email.trim().toLowerCase();
+    // if (valid[0]=== '@' || valid[valid.length-1] === '@') {
+    //     console.log('Wrong Address');
+    // }else if ( valid.split('@').length !== 2){
+    //     console.log('Wrong Address');
+    // }else if ((email.indexOf('.') - email.indexOf('@')) < 3) {
+    //     console.log('Wrong Address');
+    //
+    // }else{
+    //     console.log('Good Address');
+    // }
+
+    //Варіант 2
+    // let splited=email.split('@');
+    // if(email.includes('@')&&splited.length===2&&!(splited[1][0].includes('.')||splited[1][1].includes('.'))) {
+    //     console.log('Valid')
+    // }
+    // else console.log('Ne valid')
+}
+validator('someemail@gmail.com')
+
+
+//
+// - є масивlet
+coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'python core',
+            'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'react',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'node.js',
+            'python',
+            'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+
+//
+// відсортувати його в спадаючому порядку за кількістю елементів в полі modules
+
+let sortCourse = coursesArray.sort((subject1, subject2) => subject2.modules.length - subject1.modules.length);
+console.log(sortCourse);
+
+//
+// - Напишіть функцію count(str, stringsearch), яка повертає кількість символів stringsearch у рядку str.
+//     let symb = "о", str = "Астрономия это наука о небесных объектах";
+// document.writeln(count(str, symb)) // 5
+let count = (str, stringsearch) => {
+    let countSymbol = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === stringsearch){
+            countSymbol += 1;
+        }
+    }
+    return countSymbol;
+}
+console.log(count('Астрономия это наука о небесных объектах', 'о'));
+
+
+//
+// - Напишіть функцію cutString(str, n), яка видаляє зайві слова з рядка str, залишивши у ній n слів.
+//     let str = "Сила тяжести приложена к центру масс тела";
+// document.writeln(cutString(str, 5)) // 'Сила тяжести приложена к центру'
+let cutString2 = (str, n) =>{
+    let splited=str.split(' ');
+    let b=str.split(' ').slice(0,splited.length-n).join(' ')
+    console.log(b)
+
+}
+cutString2('Сила тяжести приложена к центру масс тела', 2);// 'Сила тяжести приложена к центру'
+
+
+//
+//
+// -стоврити масив книжок (назва, кількість сторінок, автори , жанри).
+let books=[
+    {name: 'The Alchemist',pages:250,authors:['Paulo Coelho'],genres:['live']},
+    {name: 'The Auctioneer',pages:450,authors:['Joan Samson'],genres:['horror']},
+    {name: 'War and peace',pages:1550,authors:['Tolstoj','jakubowicz'],genres:['War','Peace']},
+]
+//[ -знайти наібльшу книжку.
+// - знайти книжку/ки з найбільшою кількістю жанрів
+// - знайти книжку/ки з найдовшою назвою
+// - знайти книжку/ки які писали 2 автори
+// - знайти книжку/ки які писав 1 автор] це ми робили у lesson4 додаткові
+
+// - вісортувати книжки по кількості сторінок по зростанню
+console.log(books.sort((book1, book2) => book1 - book2));
