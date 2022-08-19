7b01d3a3c9e651c006b4cd2fcff265d2b5313b09
Konanets <ivankonanezs4@gmail.com>
2022-08-19 18:56:40 +0300

    lesson9 addition

--- lesson9/Addition/script.js
+++ lesson9/Addition/script.js
@@ -1,13 +1,12 @@
 // написати рекурсивну функцію, яка збирає всі назви класів з файлу rules.html в окремий масив. масив вивести в консоль
 
-let Classes = []
+let classes = []
 function searchClass(element) {
         if(element.className){
-            for (const classElement of element.className.split(' ')) {
-                if(!Classes.includes(classElement)){
-                    Classes.push(classElement);
-                }
-            }
+            // for (const classElement of element.className.split(' ')) {
+            //     сlasses.push(classElement);
+            // }
+            classes.push(...element.className.split(' '))
         }
     if (element.children.length) {
         for (const dytyna of element.children) {
@@ -16,4 +15,4 @@ function searchClass(element) {
     }
 }
 searchClass(document.body)
-console.log(Classes)
+console.log(classes)
